80f9f2ce3529f61ce6b26b7800e01064be6df498
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-23 12:41:12 +0100

    Initial Commit

---
+++ CoffeeApp2/CoffeeApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp2App.swift
+//  CoffeeApp2
+//
+//  Created by Sunjay Kalsi on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
